87faf16253138290d303955b61c9dc8e8552472c
Sebastian Werner <s.werner@sebastian-software.de>
2016-03-03 16:15:34 +0100

    More work on initial version

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-var postcss = require('postcss');
+import postcss from "postcss";
 
-module.exports = postcss.plugin('postcss-layout-selector', function (opts) {
-    opts = opts || {};
+function nodesToOptions(rule)
+{
+  var options = {};
+  if (rule.nodes)
+  {
+    rule.nodes.forEach(function(child) {
+      options[child.prop] = child.value;
+    });
+  }
 
-    // Work with options here
+  return options;
+}
 
-    return function (css, result) {
+export default postcss.plugin("postcss-layout-selector", function layoutSystem(pluginOptions)
+{
+  pluginOptions = pluginOptions || {};
 
-        // Transform CSS AST here
+  var currentLayout = pluginOptions.layout;
 
-    };
+  function convert(origin, name, options)
+  {
+    options = options || {};
+
+    if (name === currentLayout)
+    {
+      var parent = origin.parent;
+      var children = origin.nodes;
+
+      origin.replaceWith(origin.nodes);
+    }
+    else
+    {
+      origin.remove();
+    }
+  }
+
+  return function(css)
+  {
+    css.walkAtRules("layout", function(rule)
+    {
+      var params = postcss.list.space(rule.params);
+      var options = nodesToOptions(rule);
+
+      convert(rule, params[0], options);
+    });
+  };
 });
