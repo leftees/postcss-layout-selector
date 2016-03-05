9a89e6d7124f4cdade8155462d1f23b5af1d49bd
Sebastian Werner <s.werner@sebastian-software.de>
2016-03-05 18:36:35 +0100

    More checks for eslint

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ import postcss from "postcss"
 import plugin from "./src"
 
 function run(t, input, output, opts = { }) {
-  return postcss([ plugin(opts) ]).process(input)
-    .then( result => {
+  return postcss([ plugin(opts) ]).process(input).
+    then( result => {
       t.same(result.css, output)
       t.same(result.warnings().length, 0)
     })
