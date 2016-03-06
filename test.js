2d615311166e1ac0b58b92d1f2d737a4b27c660c
Sebastian Werner <s.werner@sebastian-software.de>
2016-03-06 13:39:32 +0100

    Using parens for parameters in arrow functions

--- test.js
+++ test.js
@@ -10,19 +10,19 @@ import plugin from "./src"
 
 function run(t, input, output, opts = { }) {
   return postcss([ plugin(opts) ]).process(input).
-    then(result => {
+    then((result) => {
       t.same(result.css, output)
       t.same(result.warnings().length, 0)
     })
 }
 
-test("Accepting matching layout", t => {
+test("Accepting matching layout", (t) => {
   return run(t, "@layout landscape{ color: red }", "color: red", {
     layout: "landscape"
   })
 })
 
-test("Removing non-matching layout", t => {
+test("Removing non-matching layout", (t) => {
   return run(t, "@layout landscape{ color: blue }", "", {
     layout: "portrait"
   })
